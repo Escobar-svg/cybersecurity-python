1065dad3056bcf15bf6e7f7cb60d18a4ff3bd71e
unknown <arnoldkibet009@gmail.com>
2026-04-10 20:49:18 +0300

    updated login system with attempt limiter

--- login.py
+++ login.py
@@ -5,19 +5,19 @@ correct_password = "12345"
 
 attempts = 3
 
+print("=== Secure Login System ===")
+
 while attempts > 0:
-    print("\n=== LOGIN SYSTEM ===")
-    
-    username = input("Enter username: ")
-    password = input("Enter password: ")
+    username = input("Username: ")
+    password = input("Password: ")
 
     if username == correct_username and password == correct_password:
-        print("Access Granted")
+        print("Access Granted ✅")
         break
     else:
         attempts -= 1
-        print("Access Denied")
-        print("Attempts left:", attempts)
+        print("Access Denied ❌")
+        print(f"Attempts left: {attempts}")
 
 if attempts == 0:
-    print("Account Locked")
+    print("Account locked 🔒")
